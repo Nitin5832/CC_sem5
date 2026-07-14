c54ff9b416ffd0e7df169e50471b9c2d42fd73cb
Nitin Rawat <rajputnitin5832@gmail.com>
2026-07-14 12:01:27 +0530

    Time: 21 ms (50.21%) | Memory: 27.4 MB (6.29%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -3,6 +3,4 @@ class Solution(object):
         for i in range(len(nums)):
             if len(nums)==len(set(nums)):
                 return False
-            for i in range(len(nums)):
-                if nums[i] in nums[(i+1):(len(nums))]:
-                    return True
+            return True
